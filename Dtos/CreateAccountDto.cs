4aefc933c415451b85d1622ba3481dbd5d35e08e
franjo3003 <franjo3003@gmail.com>
2025-12-09 23:24:01 +0100

    Dodani novi fajlovi DB_Script.sql i CreateAccountDto

---
+++ Dtos/CreateAccountDto.cs
@@ -0,0 +1,18 @@
+﻿namespace myclinic_back.Dtos
+{
+    public class CreateAccountDto
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public int? RoleId { get; set; }
+
+        public string EmailAddres { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public int SpecializationId { get; set; }
+
+    }
+}
